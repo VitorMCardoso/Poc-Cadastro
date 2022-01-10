e4863711bcdcfba5244aa7b64b8f75b0eb206b55
vitor <vitor.cardoso@finnet.com.br>
2022-01-10 19:39:17 -0300

    AJust incoming Record type

--- src/main/java/br/com/bee4/infra/streaming/consumer/IntermediaryConsumer.java
+++ src/main/java/br/com/bee4/infra/streaming/consumer/IntermediaryConsumer.java
@@ -1,7 +1,5 @@
 package br.com.bee4.infra.streaming.consumer;
 
-import br.com.bee4.domain.intermediary.model.Intermediary;
-import br.com.bee4.domain.investor.model.Investor;
 import io.smallrye.reactive.messaging.kafka.Record;
 import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -15,7 +13,7 @@ public class IntermediaryConsumer {
     private final Logger logger = Logger.getLogger(IntermediaryConsumer.class);
 
     @Incoming("intermediary-in")
-    public void receive(Record<Investor, String> record) {
+    public void receive(Record<Long, String> record) {
         logger.infof("HEREEE =>>>>>  %d - %s", record.key(), record.value());
     }
 
